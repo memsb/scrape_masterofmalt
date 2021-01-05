b864235cd921730531b2604da6f459e60dc93e1a
Martin Buckley <mbuckley@gmail.com>
2021-01-05 04:15:39 +0000

    include appspec in build artefact

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 rm -rf build
 mkdir -p build/src
 cp *.py build/src
+cp appspec.yml build/src
 pipenv lock -r > build/requirements.txt
 pip3 install -r build/requirements.txt --no-deps -t build/src
 cd build/src
