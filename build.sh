d39084c62a517218a5b652069ceca85a822ed531
Martin Buckley <mbuckley@gmail.com>
2021-01-06 03:19:23 +0000

    zip build

--- build.sh
+++ build.sh
@@ -5,3 +5,5 @@ cp *.py build/src
 cp appspec.yml build/src
 pipenv lock -r > build/requirements.txt
 pip3 install -r build/requirements.txt --no-deps -t build/src
+cd build/src
+zip -r ../output.zip .
